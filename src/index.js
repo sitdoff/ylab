290da68373ec103114eaebac67e946b688e56f19
Roman Ivanov <sitdoff@gmail.com>
2024-09-03 17:44:18 +0300

    update index.js

--- src/index.js
+++ src/index.js
@@ -2,14 +2,11 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
-import { BrowserRouter, HashRouter } from "react-router-dom";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <BrowserRouter baseName="/ylab">
-      <App />
-    </BrowserRouter>
+    <App />
   </React.StrictMode>,
 );
 
